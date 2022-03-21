3edbd290e5ff9a5cc77f186f0cbd5c6d0eea74d0
Cheolgu Kim <cgkim@nowcom.com>
2022-03-21 00:57:12 -0700

    6.4.2

---
+++ src/StarRating.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { createArray } from './lib';
+import Star from './Star';
+
+export default function StarRating({totalStars = 5, selectedStars = 0, onRate = f => f}) {
+    return (
+        <React.Fragment>
+            {createArray(totalStars).map((n, i) => (
+                <Star key={i} selected={selectedStars > i} onSelect={() => onRate(i + 1)} />
+            ))}
+            <p>
+                {selectedStars} of {totalStars} stars
+            </p>
+        </React.Fragment>
+    );
+}
